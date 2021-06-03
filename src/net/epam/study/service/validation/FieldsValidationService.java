3197dd6f5eb48645d8562acfc749fd793f4fbffb
Kirill Mordas <46963005+MordasKirill@users.noreply.github.com>
2021-06-04 00:23:24 +0300

    Delete FieldsValidationService.java

--- src/net/epam/study/service/validation/FieldsValidationService.java
+++
@@ -1,8 +0,0 @@
-package net.epam.study.service.validation;
-
-public interface FieldsValidationService {
-    String emailErrorMsg(String email);
-    String fullNameErrorMsg(String fullName);
-    String phoneErrorMsg(String phone);
-    String cityErrorMsg(String city);
-}
