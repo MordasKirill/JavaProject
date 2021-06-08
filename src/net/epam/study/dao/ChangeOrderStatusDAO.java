54ce774995f68528c26d08406bb5ed7668ee652e
Kirill Mordas <46963005+MordasKirill@users.noreply.github.com>
2021-06-08 16:48:23 +0300

    Delete ChangeOrderStatusDAO.java

--- src/net/epam/study/dao/ChangeOrderStatusDAO.java
+++
@@ -1,7 +0,0 @@
-package net.epam.study.dao;
-
-import net.epam.study.dao.connection.ConnectionPoolException;
-
-public interface ChangeOrderStatusDAO {
-    void accept(String id, String status) throws DAOException, ConnectionPoolException;
-}
