e3853ea25472ef1d40ff3227f1bf6a39a0866e4f
Kirill Mordas <46963005+MordasKirill@users.noreply.github.com>
2021-06-11 12:40:13 +0300

    Delete DeleteOrderDAO.java

--- src/net/epam/study/dao/DeleteOrderDAO.java
+++
@@ -1,7 +0,0 @@
-package net.epam.study.dao;
-
-import net.epam.study.dao.connection.ConnectionPoolException;
-
-public interface DeleteOrderDAO {
-    void delete(String id) throws DAOException, ConnectionPoolException;
-}
