1761ac0ee57c8a0268acbf9861381089e0fa7c6a
Kirill Mordas <46963005+MordasKirill@users.noreply.github.com>
2021-06-14 16:16:26 +0300

    Delete OrderCreateService.java

--- src/net/epam/study/service/OrderCreateService.java
+++
@@ -1,9 +0,0 @@
-package net.epam.study.service;
-
-import java.math.BigDecimal;
-
-public interface OrderCreateService {
-    void create(String fullName, String address, String email, String phone, StringBuilder stringBuilder) throws ServiceException;
-    void payment(String login, BigDecimal total, String status) throws ServiceException;
-    int getUserId(String login) throws ServiceException;
-}
