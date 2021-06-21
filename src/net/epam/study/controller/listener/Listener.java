2a739c7ead6edd66b5e853ed2fa5affc368f67bb
Kirill Mordas <kirill.mordas@gmail.com>
2021-06-21 21:58:54 +0300

    CP dispose action added when contextDestroyed

--- src/net/epam/study/controller/listener/Listener.java
+++ src/net/epam/study/controller/listener/Listener.java
@@ -1,22 +1,27 @@
 package net.epam.study.controller.listener;
 
 import net.epam.study.dao.connection.ConnectionPool;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class Listener implements ServletContextListener {
 
-    //public static final String driver = "com.mysql.jdbc.Driver";
+    private static final Logger log = Logger.getLogger(Listener.class);
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-        System.out.println("ServletContextListener was created!");
+
+        log.log(Level.INFO, "ServletContextListener was created!");
         ConnectionPool.connectionPool = new ConnectionPool();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
-        System.out.println("ServletContextListener was destroyed!");
+
+        log.log(Level.INFO, "ServletContextListener was destroyed!");
+        ConnectionPool.connectionPool.dispose();
     }
 }
