dcbd91f4b26ac4e41e9b40ca3a7e0239f224c3d2
Kirill Mordas <46963005+MordasKirill@users.noreply.github.com>
2021-08-03 23:29:47 +0300

    Delete CheckSessionDAO.java

--- src/net/epam/study/dao/CheckSessionDAO.java
+++
@@ -1,7 +0,0 @@
-package net.epam.study.dao;
-
-public interface CheckSessionDAO {
-    boolean isSession(Boolean auth, String role);
-    boolean isAdmin(String role);
-    boolean isUser(String role);
-}
