14c48fb8a58f14f168349af62eaa059cdb4dd0da
Kirill Mordas <46963005+MordasKirill@users.noreply.github.com>
2021-08-03 23:31:23 +0300

    Delete CheckSessionService.java

--- src/net/epam/study/service/CheckSessionService.java
+++
@@ -1,7 +0,0 @@
-package net.epam.study.service;
-
-public interface CheckSessionService {
-    boolean checkSession(Boolean auth, String role);
-    boolean checkAdmin(String role);
-    boolean checkUser(String role);
-}
