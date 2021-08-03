bef4648f8abe6d6018fc86f1cd65dad1ff00d66c
Kirill Mordas <46963005+MordasKirill@users.noreply.github.com>
2021-08-03 23:31:49 +0300

    Delete ChangeTableInfoService.java

--- src/net/epam/study/service/ChangeTableInfoService.java
+++
@@ -1,7 +0,0 @@
-package net.epam.study.service;
-
-public interface ChangeTableInfoService {
-    void changeStatus(String id, String status) throws ServiceException;
-    void changeRole(String id, String role) throws ServiceException;
-    void changePaymentStatus(String status, String login) throws ServiceException;
-}
