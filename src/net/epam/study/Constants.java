7935ab107c8877a8e6a0d56db342e8cfad324c03
Kirill Mordas <46963005+MordasKirill@users.noreply.github.com>
2021-08-09 17:18:58 +0300

    Update Constants.java

--- src/net/epam/study/Constants.java
+++ src/net/epam/study/Constants.java
@@ -23,6 +23,5 @@ public class Constants {
     public static final String PARAM_EMAIL = "email";
     public static final String ID_ORDER = "idOrder";
     public static final String ID_USER = "idUser";
-    public static final List<MenuItem> ORDER = new ArrayList<>();
     public static final int DEFAULT_LIMIT = 8;
 }
